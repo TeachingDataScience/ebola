18593b026486be98bde003183e6bcbf147f92160
King Zack <zacharydesario@gmail.com>
2014-11-01 17:29:13 -0700

    adding research documents and notes

---
+++ zd-research-notes/data-lineage.py
@@ -0,0 +1,5 @@
+wikipedia -> w.h.o -> ministrys of health [google]-> http://www.health.gov.ng/
+
+
+http://www.mohsw.gov.lr/documents/Liberia%20Ebola%20SitRep%2097%20Aug%2020,%202014.pdf
+http://www.mohsw.gov.lr/documents/Liberia%20Ebola%20SitRep%2094%20Aug%2017,%202014.pdf
